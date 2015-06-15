a7adb6036de5fa68469ebf7a05cbfa67a355a023
buddybuild <help@buddybuild.com>
2015-06-15 13:10:53 -0700

    Adding BuddyBuildSDK

--- Anypic-iOS/Anypic/AppDelegate.m
+++ Anypic-iOS/Anypic/AppDelegate.m
@@ -17,6 +17,7 @@
 #import "PAPWelcomeViewController.h"
 #import "PAPActivityFeedViewController.h"
 #import "PAPPhotoDetailsViewController.h"
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 @interface AppDelegate () {
     BOOL firstLaunch;
@@ -39,6 +40,8 @@
 #pragma mark - UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [BuddyBuildSDK setup:self];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     // ****************************************************************************
